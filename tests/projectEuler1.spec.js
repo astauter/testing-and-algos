eb9c445d220472b144c6f668362822e4d691d239
astauter <ajstauter@gmail.com>
2017-10-20 15:03:11 -0500

    project e q1 answer

--- tests/projectEuler1.spec.js
+++ tests/projectEuler1.spec.js
@@ -3,7 +3,7 @@ describe('sumOfMultiplesOfThreeAndFive', () => {
         expect(typeof sumOfMultiplesOfThreeAndFive).toEqual('function');
     });
     it('returns the sum of all the numbers of multiples of 3 or 5', () => {
-        expect(sumOfMultiplesOfThreeAndFive(10)).toEqual(23)
+        expect(sumOfMultiplesOfThreeAndFive(10)).toEqual(33)
         expect(sumOfMultiplesOfThreeAndFive(5)).toEqual(8)
     });
 });
